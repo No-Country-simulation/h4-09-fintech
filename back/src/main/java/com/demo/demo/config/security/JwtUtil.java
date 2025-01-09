e9958e73baee2ad230fddcd273ca32e4331df944
Catriel <catriellescobar@gmail.com>
2025-01-09 20:46:10 -0300

    delete:google auth

--- back/src/main/java/com/demo/demo/config/security/JwtUtil.java
+++ back/src/main/java/com/demo/demo/config/security/JwtUtil.java
@@ -3,7 +3,6 @@ package com.demo.demo.config.security;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
--- back/src/main/java/com/demo/demo/config/security/SecurityConfig.java
+++ back/src/main/java/com/demo/demo/config/security/SecurityConfig.java
@@ -1,74 +1,37 @@
 package com.demo.demo.config.security;
 
-import com.demo.demo.config.security.oauth2.*;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.converter.FormHttpMessageConverter;
+
 import org.springframework.security.authentication.AuthenticationProvider;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.client.endpoint.DefaultAuthorizationCodeTokenResponseClient;
-import org.springframework.security.oauth2.client.endpoint.OAuth2AccessTokenResponseClient;
-import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
-import org.springframework.security.oauth2.client.http.OAuth2ErrorResponseErrorHandler;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.core.http.converter.OAuth2AccessTokenResponseHttpMessageConverter;
-import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.web.access.AccessDeniedHandler;
-import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
+import org.springframework.security.web.SecurityFilterChain;
+
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-    private final static String OAUTH2_REDIRECTION_ENDPOINT = "/oauth2/callback/**";
-    private final static String OAUTH2_BASE_URI = "/api/auth/oauth2/authorize";
 
-    private final CustomOAuth2UserService customOAuth2UserService;
-    private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
-    private final ClientRegistrationRepository clientRegistrationRepository;
-    private final OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
     private final AuthenticationProvider authenticationProvider;
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers(HttpMethod.GET,OAUTH2_BASE_URI).permitAll();
                     auth.requestMatchers( "/api/auth/**").permitAll();
                     auth.requestMatchers(HttpMethod.GET,"/api/auth/check-login").hasRole("USER");
                     auth.anyRequest().authenticated();
                 })
-                .oauth2Login(oauth2 -> oauth2
-                        .authorizationEndpoint(authorizationEndpointConfig ->
-                                authorizationEndpointConfig.
-                                        baseUri("/oauth2/authorize")
-                                        .authorizationRequestResolver(
-                                                new CustomAuthorizationRequestResolver(
-                                                        clientRegistrationRepository,
-                                                        "/oauth2/authorize"))
-                        )
-                        .redirectionEndpoint(redirectionEndpointConfig ->
-                                redirectionEndpointConfig.baseUri(OAUTH2_REDIRECTION_ENDPOINT))
-                        .userInfoEndpoint(userInfoEndpointConfig ->
-                                userInfoEndpointConfig.userService(customOAuth2UserService))
-
-                        .successHandler(oAuth2AuthenticationSuccessHandler)
-                        .failureHandler(oAuth2AuthenticationFailureHandler))
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
@@ -79,32 +42,4 @@ public class SecurityConfig {
         return http.build();
     }
 
-
-    @Bean
-    public AccessDeniedHandler accessDeniedHandler() {
-        return (request, response, accessDeniedException) -> {
-            response.setStatus(HttpStatus.FORBIDDEN.value());
-            response.getWriter().write("403 Forbidden: Acceso denegado." + accessDeniedException.getMessage());
-
-        };
-    }
-    @Bean
-    public AuthenticationEntryPoint authenticationEntryPoint() {
-        return new Http403ForbiddenEntryPoint(); // This will return a 403 error if not authenticated
-    }
-
-    private OAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest> authorizationCodeTokenResponseClient() {
-        OAuth2AccessTokenResponseHttpMessageConverter tokenResponseHttpMessageConverter = new OAuth2AccessTokenResponseHttpMessageConverter();
-        tokenResponseHttpMessageConverter
-                .setAccessTokenResponseConverter(new CustomAccessTokenResponseConverter());
-
-        RestTemplate restTemplate = new RestTemplate(
-                Arrays.asList(new FormHttpMessageConverter(), tokenResponseHttpMessageConverter));
-        restTemplate.setErrorHandler(new OAuth2ErrorResponseErrorHandler());
-
-        DefaultAuthorizationCodeTokenResponseClient tokenResponseClient = new DefaultAuthorizationCodeTokenResponseClient();
-        tokenResponseClient.setRestOperations(restTemplate);
-
-        return tokenResponseClient;
-    }
 }
