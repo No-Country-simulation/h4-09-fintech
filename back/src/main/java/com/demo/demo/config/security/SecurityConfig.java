4db1f9c7fe93bead46bde432a381a56d8d437a67
Catriel <catriellescobar@gmail.com>
2025-01-10 12:34:15 -0300

    update cors

--- back/src/main/java/com/demo/demo/config/security/SecurityConfig.java
+++ back/src/main/java/com/demo/demo/config/security/SecurityConfig.java
@@ -52,10 +52,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173","*"));
+        configuration.setAllowCredentials(false);
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
