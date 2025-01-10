c368fdd056870d44d9810e7ba8f9a7f42c73ac3f
Catriel <catriellescobar@gmail.com>
2025-01-10 11:48:01 -0300

    add cors

--- back/src/main/java/com/demo/demo/config/security/SecurityConfig.java
+++ back/src/main/java/com/demo/demo/config/security/SecurityConfig.java
@@ -16,6 +16,12 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -27,6 +33,7 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers( "/api/auth/**").permitAll();
                     auth.requestMatchers(HttpMethod.GET,"/api/auth/check-login").hasRole("USER");
@@ -40,6 +47,18 @@ public class SecurityConfig {
                         UsernamePasswordAuthenticationFilter.class)
                 .securityContext(context -> context.requireExplicitSave(true));
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:5173","*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
