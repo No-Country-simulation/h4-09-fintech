966634919fb5b2cb209b052e45595809d609ff9d
BenjaMoreno <webenja0@gmail.com>
2025-01-14 21:05:13 -0300

    Modificacion para Typescript

--- front/src/views/Dashboard/(components)/graficos/Linear/LinearChart.tsx
+++ front/src/views/Dashboard/(components)/graficos/Linear/LinearChart.tsx
@@ -1,4 +1,3 @@
-import React, { PureComponent } from "react";
 import {
   LineChart,
   Line,
@@ -55,45 +54,42 @@ const data = [
   },
 ];
 
-export default class Example extends PureComponent {
-  static demoUrl =
-    "https://codesandbox.io/p/sandbox/line-chart-width-xaxis-padding-8v7952";
+const Example = () => {
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "34.68vh",
+      }}
+    >
+      <ResponsiveContainer width="100%" height="100%">
+        <LineChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend layout="horizontal" verticalAlign="top" />
+          <Line
+            type="monotone"
+            dataKey="Ingresos"
+            stroke="#0048B2"
+            activeDot={{ r: 8 }}
+          />
+          <Line type="monotone" dataKey="Egresos" stroke="#CC0003" />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
 
-  render() {
-    return (
-      <div
-        style={{
-          width: "100%",
-          height: "34.68vh",
-        }}
-      >
-        <ResponsiveContainer width="100%" height="100%">
-          <LineChart
-            width={500}
-            height={300}
-            data={data}
-            margin={{
-              top: 5,
-              right: 30,
-              left: 20,
-              bottom: 5,
-            }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Legend layout="horizontal" verticalAlign="top" />
-            <Line
-              type="monotone"
-              dataKey="Ingresos"
-              stroke="#0048B2"
-              activeDot={{ r: 8 }}
-            />
-            <Line type="monotone" dataKey="Egresos" stroke="#CC0003" />
-          </LineChart>
-        </ResponsiveContainer>
-      </div>
-    );
-  }
-}
+export default Example;
