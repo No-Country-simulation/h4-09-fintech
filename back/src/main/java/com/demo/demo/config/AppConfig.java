e1b4661c21fda5245d4aa8843409bc36ac8cbbc2
Catriel <catriellescobar@gmail.com>
2025-01-20 19:15:41 -0300

    cedears endpoints

---
+++ back/src/main/java/com/demo/demo/config/AppConfig.java
@@ -0,0 +1,22 @@
+package com.demo.demo.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Configuration
+public class AppConfig {
+//    @Bean
+//    public RestTemplate restTemplate() {
+//        return new RestTemplate();
+//    }
+    @Bean
+    public ObjectMapper objectMapper () {
+        return new ObjectMapper();
+    }
+}
