c19bfec8ba6a7953cff7afe7f6e64e2c6bc79cff
BenjaMoreno <webenja0@gmail.com>
2025-01-29 16:35:19 -0300

    cerrar notifiacion del dash

--- front/src/views/Dashboard/notificaciones/NoteDash.tsx
+++ front/src/views/Dashboard/notificaciones/NoteDash.tsx
@@ -2,10 +2,25 @@ import { Link } from "react-router-dom";
 import "./NoteDash.css";
 import { XMarkIcon } from "@heroicons/react/16/solid";
 import logo from "../../../assets/icons/(iupi)/Logo iupi 14px.svg";
+import { useRef } from "react";
 
 export const NoteDash = () => {
+  const boxNoteDash = useRef<HTMLDivElement | null>(null); // Definir el tipo correctamente
+
+  const cerrarNotificacion = () => {
+    if (boxNoteDash.current) {
+      boxNoteDash.current.style.display = "none";
+
+      setTimeout(() => {
+        if (boxNoteDash.current) {
+          boxNoteDash.current.style.display = "flex";
+        }
+      }, 3_000_000); // Puedes usar guion bajo para mejorar la legibilidad del número
+    }
+  };
+
   return (
-    <div className="container-notificacion">
+    <div className="container-notificacion" ref={boxNoteDash}>
       <img src={logo} className="logo-notificaciones" />
       <Link to="/notificaciones" className="info-notes">
         <h5>Progreso en tus alertas de inversión</h5>
@@ -14,7 +29,7 @@ export const NoteDash = () => {
           29min
         </p>
       </Link>
-      <XMarkIcon className="cerrar-not-btn" />
+      <XMarkIcon className="cerrar-not-btn" onClick={cerrarNotificacion} />
     </div>
   );
 };
