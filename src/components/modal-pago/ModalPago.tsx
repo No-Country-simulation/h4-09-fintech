e1922e8bca80881f78320147f58c347c56349bd0
Catriel <catriellescobar@gmail.com>
2025-02-06 07:28:22 -0300

    add spinner nav

--- src/components/modal-pago/ModalPago.tsx
+++ src/components/modal-pago/ModalPago.tsx
@@ -3,6 +3,8 @@ import { CircleX } from 'lucide-react';
 import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useCuentaBanco } from '../../context/CuentaBancoContext';
+import Logo from '../../assets/icons/Logo';
+import Spinner from '../Spiner';
 
 interface Props {
   setShowModal: React.Dispatch<React.SetStateAction<boolean>>;
@@ -15,7 +17,7 @@ interface form {
 }
 
 const ModalPago = ({ setShowModal }: Props) => {
-  const { cuenta, fetchCuenta, updateCuenta } = useCuentaBanco();
+  const { cuenta, fetchCuenta, updateCuenta, loading } = useCuentaBanco();
   const [defaultValues] = useState<form>({
     alias: '',
     cbu: '',
@@ -68,113 +70,99 @@ const ModalPago = ({ setShowModal }: Props) => {
           >
             <CircleX className="w-10 h-10 " />
           </div>
-          <div className="flex flex-col gap-3 mb-4 w-[100%]">
-            <div className="pt-8 text-center">
-              <div className="text-4xl font-bold pb-4">
-                <h2>Actualizar metodo de pago</h2>
-              </div>
-              <form
-                onSubmit={handleSubmit(onSubmit)}
-                className="max-w-md mx-auto bg-white p-6 rounded-lg shadow-lg space-y-4"
-              >
-                <div>
-                  <label htmlFor="alias" className="block text-gray-700 font-medium">
-                    Alias:
-                  </label>
-                  <input
-                    type="text"
-                    id="alias"
-                    placeholder="Alias"
-                    {...register('alias', { required: 'El alias es obligatorio' })}
-                    className="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                  />
-                  {errors.alias && <p className="text-red-500 text-sm">{errors.alias.message}</p>}
-                </div>
-
-                <div>
-                  <label htmlFor="cbu" className="block text-gray-700 font-medium">
-                    CBU:
-                  </label>
-                  <input
-                    type="text"
-                    id="cbu"
-                    placeholder="CBU"
-                    {...register('cbu', {
-                      required: 'El CBU es obligatorio',
-                      minLength: { value: 22, message: 'El CBU debe tener 22 dígitos' },
-                    })}
-                    className="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                  />
-                  {errors.cbu && <p className="text-red-500 text-sm">{errors.cbu.message}</p>}
-                </div>
-
-                <div>
-                  <label htmlFor="cuit" className="block text-gray-700 font-medium">
-                    CUIT:
-                  </label>
-                  <input
-                    type="text"
-                    id="cuit"
-                    placeholder="CUIT"
-                    {...register('cuit', {
-                      required: 'El CUIT es obligatorio',
-                      pattern: {
-                        value: /^[0-9]{11}$/,
-                        message: 'El CUIT debe tener 11 dígitos numéricos',
-                      },
-                    })}
-                    className="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                  />
-                  {errors.cuit && <p className="text-red-500 text-sm">{errors.cuit.message}</p>}
-                </div>
-
-                <div>
-                  <label htmlFor="titular" className="block text-gray-700 font-medium">
-                    Titular de la cuenta:
-                  </label>
-                  <input
-                    type="text"
-                    id="titular"
-                    placeholder="Titular de la cuenta"
-                    {...register('titular', { required: 'El titular es obligatorio' })}
-                    className="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                  />
-                  {errors.titular && (
-                    <p className="text-red-500 text-sm">{errors.titular.message}</p>
-                  )}
-                </div>
-
-                <button
-                  type="submit"
-                  className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition"
-                >
-                  Enviar
-                </button>
-              </form>
+          {loading ? (
+            <div className="w-full backdrop-blur-sm h-screen flex  flex-col gap-8 justify-center items-center">
+              {' '}
+              <Logo />
+              <Spinner />
             </div>
-            {/* <div className="w-full h-14 flex items-center gap-2 mt-4 relative ">
-              {false && (
-                <div className="w-full h-full flex justify-center items-center backdrop-blur-sm absolute opacity-90">
-                  <Spinner />
+          ) : (
+            <div className="flex flex-col gap-3 mb-4 w-[100%]">
+              <div className="pt-8 text-center">
+                <div className="text-4xl font-bold pb-4">
+                  <h2>Actualizar metodo de pago</h2>
                 </div>
-              )}
-              {hay ? (
-                <button
-                  className="w-full h-[80%]  bg-[#577fd6] rounded-md text-white font-bold"
-                  onClick={() => updateMetodoPago()}
+                <form
+                  onSubmit={handleSubmit(onSubmit)}
+                  className="max-w-md mx-auto bg-white p-6 rounded-lg shadow-lg space-y-4"
                 >
-                  Actualizar metodo de pago
-                </button>
-              ) : (
-                <button
-                  className="w-full h-[80%]  bg-[#577fd6] rounded-md text-white font-bold"
-                  onClick={() => updateMetodoPago()}
-                >
-                  Crear metodo de pago
-                </button>
-              )}
-            </div> */}
-          </div>
+                  <div>
+                    <label htmlFor="alias" className="block text-gray-700 font-medium">
+                      Alias:
+                    </label>
+                    <input
+                      type="text"
+                      id="alias"
+                      placeholder="Alias"
+                      {...register('alias', { required: 'El alias es obligatorio' })}
+                      className="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    />
+                    {errors.alias && <p className="text-red-500 text-sm">{errors.alias.message}</p>}
+                  </div>
+
+                  <div>
+                    <label htmlFor="cbu" className="block text-gray-700 font-medium">
+                      CBU:
+                    </label>
+                    <input
+                      type="text"
+                      id="cbu"
+                      placeholder="CBU"
+                      {...register('cbu', {
+                        required: 'El CBU es obligatorio',
+                        minLength: { value: 22, message: 'El CBU debe tener 22 dígitos' },
+                      })}
+                      className="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    />
+                    {errors.cbu && <p className="text-red-500 text-sm">{errors.cbu.message}</p>}
+                  </div>
+
+                  <div>
+                    <label htmlFor="cuit" className="block text-gray-700 font-medium">
+                      CUIT:
+                    </label>
+                    <input
+                      type="text"
+                      id="cuit"
+                      placeholder="CUIT"
+                      {...register('cuit', {
+                        required: 'El CUIT es obligatorio',
+                        pattern: {
+                          value: /^[0-9]{11}$/,
+                          message: 'El CUIT debe tener 11 dígitos numéricos',
+                        },
+                      })}
+                      className="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    />
+                    {errors.cuit && <p className="text-red-500 text-sm">{errors.cuit.message}</p>}
+                  </div>
+
+                  <div>
+                    <label htmlFor="titular" className="block text-gray-700 font-medium">
+                      Titular de la cuenta:
+                    </label>
+                    <input
+                      type="text"
+                      id="titular"
+                      placeholder="Titular de la cuenta"
+                      {...register('titular', { required: 'El titular es obligatorio' })}
+                      className="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    />
+                    {errors.titular && (
+                      <p className="text-red-500 text-sm">{errors.titular.message}</p>
+                    )}
+                  </div>
+
+                  <button
+                    type="submit"
+                    className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition"
+                  >
+                    Enviar
+                  </button>
+                </form>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
