e0c393abc20b098ad1b8c8aaf65c03614c70cc8e
Catriel <catriellescobar@gmail.com>
2025-02-06 06:33:57 -0300

    allow

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
+    allowedHosts: ['h4-09-fintech-production-53e6.up.railway.app'],
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'h4-09-fintech-production-53e6.up.railway.app',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
